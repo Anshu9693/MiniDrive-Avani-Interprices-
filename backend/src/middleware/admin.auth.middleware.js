92158e7728745d66424c1914a8a1f90fbe01cbfb
Anshu9693 <anshukumarsah8585@gmail.com>
2026-01-13 10:04:39 +0530

    admin middleware changed

--- backend/src/middleware/admin.auth.middleware.js
+++ backend/src/middleware/admin.auth.middleware.js
@@ -1,30 +1,20 @@
-import jwt from "jsonwebtoken";
-import AdminModel from "../models/admin.model.js";
-
 export const adminAuthMiddleware = async (req, res, next) => {
   try {
-    if (!req.cookies || !req.cookies.token) {
-      return res.status(401).json({ message: "Token missing" });
-    }
-
     const token = req.cookies.token;
-
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
     if (decoded.role !== "admin") {
       return res.status(403).json({ message: "Admin access only" });
     }
-
     const admin = await AdminModel.findById(decoded.userId).select("-password");
-
     if (!admin) {
-      return res.status(401).json({ message: "Admin not found" });
+      return res.status(401).json({ message: "Invalid token" });
     }
-
     req.admin = admin;
     next();
   } catch (error) {
-    console.error("Admin auth error:", error.message);
     return res.status(401).json({ message: "Invalid or expired token" });
   }
 };
